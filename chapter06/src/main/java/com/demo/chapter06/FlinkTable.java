41ef86085946ab5f7b55b399e1d68865f35a14cd
Tanmay Deshpande <tanmay.avinash.deshpande@gmail.com>
2016-09-24 19:09:45 +0530

    Intial Commit

---
+++ chapter06/src/main/java/com/demo/chapter06/FlinkTable.java
@@ -0,0 +1,45 @@
+package com.demo.chapter06;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.table.BatchTableEnvironment;
+import org.apache.flink.api.table.Table;
+import org.apache.flink.api.table.TableEnvironment;
+
+public class FlinkTable {
+
+	public static void main(String[] args) throws Exception {
+
+		// set up execution environment
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		BatchTableEnvironment tEnv = TableEnvironment.getTableEnvironment(env);
+
+		DataSet<WC> input = env.fromElements(new WC("Hello", 1), new WC("Ciao", 1), new WC("Hello", 1));
+
+		// register the DataSet as table "WordCount"
+		tEnv.registerDataSet("WordCount", input, "word, frequency");
+
+		// run a SQL query on the Table and retrieve the result as a new Table
+		Table table = tEnv.sql("SELECT word, SUM(frequency) as frequency FROM WordCount GROUP BY word");
+
+		DataSet<WC> result = tEnv.toDataSet(table, WC.class);
+
+		result.print();
+	}
+
+	public static class WC {
+		public String word;
+		public long frequency;
+
+		public WC(String word, long frequency) {
+			this.word = word;
+			this.frequency = frequency;
+		}
+
+		@Override
+		public String toString() {
+			return "WC " + word + " " + frequency;
+		}
+	}
+
+}
